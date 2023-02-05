7e32de300c986a36377436c3df038125a80577e9
Avondeil <123551851+Avondeil@users.noreply.github.com>
2023-02-05 22:22:23 +0500

    Zad3.kt

--- src/Zad3.kt
+++ src/Zad3.kt
@@ -11,23 +11,22 @@ fun main (){
     var kol2 = readLine()!!.toDouble()
     var sum=0
     var a:Double
-    if (kol1<kol2)
-    {
-    for (g in kol1.toInt()..kol2.toInt())
-    {
-       if (g==1)
-       {
-           a=K
-           sum= (sum + a).toInt();
-       }
-        if (g>1)
-        {
-            a=K*Math.pow(P, (g-1).toDouble())
-            sum=(sum + a).toInt();
+        if (kol1>0 && kol2>0) {
+            if (kol1 < kol2) {
+                for (g in kol1.toInt()..kol2.toInt()) {
+                    if (g == 1) {
+                        a = K
+                        sum = (sum + a).toInt();
+                    }
+                    if (g > 1) {
+                        a = K * Math.pow(P, (g - 1).toDouble())
+                        sum = (sum + a).toInt();
+                    }
+                }
+                println(sum)
+            } else print("Номер 1 должен быть меньше 2 номера (num1<num2)")
         }
-    }
-    println(sum)}
-    else print("Номер 1 должен быть меньше 2 номера (num1<num2)")
+        else print("Номера не могут быть отрицательным числом")
 
     } catch (e:Exception){
         println("Неккоректный ввод данных!")
